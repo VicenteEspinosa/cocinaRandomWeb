35964389543299c86d7c05cef17a385ed97a7321
Vicente Espinosa <vnespinosa@uc.cl>
2021-08-12 00:48:48 -0400

    feat(recipes): added recipe indexing and recipe style

--- src/App.js
+++ src/App.js
@@ -6,37 +6,53 @@ const App = () => {
 
   const URL = "https://cocina-random-backend.herokuapp.com";
   //const URL = "http://127.0.0.1:8000/"
-    const [recipes, setRecipes] = useState([])
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
 
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, [query])
 
   const getRecipes = async () => {
-    const response = await fetch(`${URL}/recipes/`);
+    const response = await fetch(`${URL}/recipes_paginated/?page_size=20&categories=&ingredients=&name=${query}`);
     const data = await response.json();
-    console.log(data)
-    setRecipes(data)
+    setRecipes(data.data);
+  };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
   }
 
   return(
     <div className="App">
-      <form className="search-form">
-        <h1>Hello React</h1>
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">
           Buscar
         </button>
       </form>
+      <div className="recipes">
       {recipes.map(recipe=>(
         <Recipe
           key={recipe.id}
           title={recipe.name}
           description={recipe.description}
           image={recipe.image}
+          ingredients={recipe.ingredients}
+          categories={recipe.categories}
+          links={recipe.links}
         />
       ))}
+      </div>
     </div>
   );
 };
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import style from './recipe.module.css';
 
-const Recipe = ({title, description, image}) => {
+const Recipe = ({title, description, image, ingredients, categories, links}) => {
+
     return(
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
             <p>{description}</p>
-            <img src={image} alt="" />
+            <img className={style.image} src={image} alt="" />
+            <ol className="ingredient-container">
+                {ingredients.map(ingredient => (
+                    <li key={ingredient.id}>{ingredient.name}</li>
+                ))}
+            </ol>
+            <ol className="category-container">
+                {categories.map(category => (
+                    <li key={category.id}>{category.name}</li>
+                ))}
+            </ol>
+            <ol className="link-container">
+                {links.map(link => (
+                    <a key={link} href={link}>{link}</a>
+                ))}
+            </ol>
         </div>
     );
-}
+};
 
 export default Recipe
